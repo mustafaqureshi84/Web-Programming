2db9f93e266536eceb09b7008a2c51ee7553a4a6
mustafaqureshi84 <mustafaqureshi818@gmail.com>
2023-04-05 11:10:20 +0500

    T9

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,5 +1,24 @@
-const viewProducts = (req, res) => {
-    res.send("View Products Page");
+const { Op, product } = require('../models');
+
+const viewProducts = async (req, res) => {
+    let { page, brand, price, sort, sorder, search } = req.query;
+    if (price) {
+        priceRange = price.split(',');
+    }
+    if (sort) {
+        sortFilter = sort.split(',');
+
+    }
+    let products = await product.findAll({
+        where: {
+            brand: brand || { [Op.ne]: null },
+            price: price ? { [Op.gte]: Number(priceRange[0]), [Op.lte]: Number(priceRange[1]) } : { [Op.ne]: null },
+            name: search ? { [Op.like]: `%${search}%` } : { [Op.ne]: null }
+        },
+        order: sort ? sortFilter.map((attri) => [attri, sorder]) : null,
+        limit: 5, offset: (Number(page) - 1) * 5
+    });
+    res.status(200).json({ length: products.length, products });
 };
 
 const addProducts = (req, res) => {
